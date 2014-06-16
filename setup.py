09d47ce397a7a6676b5d75468c71a8e6e818889a
kungfulucky7 <terrywong283@live.com>
2014-06-15 21:21:31 -0700

    requirements change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 import os
 
 # Put here required packages
-packages = ['Django<=1.6', 'GDAL', 'Pillow', 'Pygments', 'South', 'argparse', 'diff-match-patch', 'dj-database-url', 'dj-static', 'django-appconf', 'django-filebrowser', 'django-geoposition', 'django-grappelli', 'django-leaflet', 'django-toolbelt', 'djangorestframework', 'docutils', 'geopy', 'gunicorn', 'psycopg2', 'pystache', 'requests', 'six', 'static', 'tablib', 'vboxapi', 'virtualenv', 'wsgiref',]
+packages = ['Django<=1.6', 'GDAL', 'Pillow', 'Pygments', 'South', 'argparse', 'diff-match-patch', 'dj-database-url', 'dj-static', 'django-appconf', 'django-filebrowser', 'django-geoposition', 'django-grappelli', 'django-leaflet', 'django-toolbelt', 'djangorestframework', 'docutils', 'geopy', 'gunicorn', 'psycopg2', 'pystache', 'requests', 'six', 'static', 'tablib', 'virtualenv', 'wsgiref',]
 
 if 'REDISCLOUD_URL' in os.environ and 'REDISCLOUD_PORT' in os.environ and 'REDISCLOUD_PASSWORD' in os.environ:
      packages.append('django-redis-cache')
